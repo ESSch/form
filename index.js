f552cf1d2f5939f333e5b3e43aa558af0f210eab
e.shtolc <e.shtolc@chtp.net>
2017-09-01 22:11:25 +0300

    add moderate input

--- index.js
+++ index.js
@@ -8,26 +8,55 @@ var formView = {
     form: null,
 
     valid: function (name, status) {
-        var field = this[name];
-        if (!status) {
-            field.className += " error";
-        } else {
-            field.value = '';
+        if (null !== this[name]) {
+            var field = this[name];
+            if (!status) {
+                field.className = "error";
+                field.value = '';
+            } else {
+                field.className = "";
+            }
         }
     },
+
+    // validate: function () {
+    //     fields = [];
+    //     for (var field in this.form) {
+    //         var bValid = !window["moderate" + field.charAt(0).toUpperCase() + field.substr(1)](this.form[field]);
+    //         if (bValid) {
+    //             fields.push(field);
+    //         }
+    //         console.log(field);
+    //     }
+    //     return {"isValid": fields.length == 0, "errorFields": fields};
+    // },
 
     send: function (e) {
         event.preventDefault ? event.preventDefault() : (event.returnValue = false);
         var form =  e.target.closest("form");
         formView.url = form.getAttribute("action");
-        formView.fio = form.querySelector("input[name=fio]");
-        formView.email = form.querySelector("input[name=email]");
-        formView.phone = form.querySelector("input[name=phone]");
 
-        // e.target.disabled = true;
-        formView.request(formView.url);
-        formView.showResult();
-        // formView.valid('phone', false);
+        var status = true;
+        var fields = [];
+        ["fio", "email", "phone"].forEach(function (name) {
+            formView[name] = form.querySelector("input[name=" + name + "]");
+            var nameMethod = "moderate" + name.charAt(0).toUpperCase() + name.substr(1);
+            var value =  formView[name].value;
+            var bValid = window[nameMethod](value);
+            console.log(value);
+            console.log(bValid);
+            if (bValid) {
+                fields.push(name);
+            }
+            status = status && bValid;
+            formView.valid(name, bValid);
+        });
+
+        if (status) {
+            e.target.disabled = true;
+            formView.request(formView.url);
+            formView.showResult();
+        }
 
         return false;
     },
@@ -98,38 +127,45 @@ document.getElementById('submitButton').onclick = formView.send;
 // formView.request("success.json");
 // setTimeout(2000);
 // console.log('success' == formView.status);
-
+//
 // formView.request("progress.json");
 // setTimeout(2000);
 // console.log('progress' == formView.status);
 //
 // setTimeout(3000);
 // console.log('success' == formView.status);
-/*
- var MyForm = {
- form: {
- fio: "",
- email: "",
- phone: ""
- },
- validate: function () {
- fields = [];
- for (field in this.form) {
- if(!window["moderate" + field.charAt(0).toUpperCase() + field.substr(1)](this.form[field])) {
- fields.push(field);
- }		}
- return {"isValid": fields.length == 0, "errorFields": fields};
- },
- getData: function () {
- return this.form;
- },
- setData: function (data) {
- this.form = data;
- },
- submit: function () {
- }
- };
+var MyForm = {
+    view: formView,
+    form: {
+        fio: "",
+        email: "",
+        phone: ""
+    },
+    validate: function () {
+        fields = [];
+        for (var field in this.form) {
+            var bValid = !window["moderate" + field.charAt(0).toUpperCase() + field.substr(1)](this.form[field]);
+            if (bValid) {
+                fields.push(field);
+            }
+            console.log(field);
+            this.view.valid(field, false);
+        }
+        return {"isValid": fields.length == 0, "errorFields": fields};
+    },
+    getData: function () {
+        return this.form;
+    },
+    setData: function (data) {
+        this.form = data;
+    },
+    submit: function () {
+        // MyForm.validate();
+    }
+};
 
+MyForm.validate();
+/*
  function eqObj(obj1, obj2) {
  b = (obj1.isValid == obj2.isValid);
  b = b && ((obj1.errorFields + " ") == (obj2.errorFields + " "));
@@ -164,65 +200,71 @@ document.getElementById('submitButton').onclick = formView.send;
  document.getElementById("submitButton").disabled = true;
  e.preventDefault();
  };
+*/
+function moderateFio(name) {
+    if (undefined == name) {
+        return false;
+    }
+    return null !== name.match(/^\s*(\S+\s+){2}\S+\s*$/);
+}
 
- function moderateFio(name)
- {
- return null !== name.match(/^\s*(\S+\s+){2}\S+\s*$/);
- }
+console.log(moderateFio("i i i"));
+console.log(moderateFio(" i i i "));
+console.log(moderateFio("Штльц Евгений Сергеевич"));
+console.log(!moderateFio(""));
+console.log(!moderateFio("i i i i"));
+//console.log(!moderateName("i i i"));
 
- console.log(moderateFio("i i i"));
- console.log(moderateFio(" i i i "));
- console.log(moderateFio("Штльц Евгений Сергеевич"));
- console.log(!moderateFio(""));
- console.log(!moderateFio("i i i i"));
- //console.log(!moderateName("i i i"));
+function moderateEmail(mail) {
+    if (undefined == mail) {
+        return false;
+    }
+    if (null === mail.match(/.+\@/)) {
+        return false;
+    }
 
- function moderateEmail(mail)
- {
- if(null === mail.match(/.+\@/)) { // replace mailregex.ru
- return false;
- }
+    if (null === mail.match(/(ya\.ru|yandex\.(ru|ua|by|kz|com))$/)) {
+        return false;
+    }
 
- if (null === mail.match(/(ya\.ru|yandex\.(ru|ua|by|kz|com))$/)) {
- return false;
- }
+    return true;
+}
 
- return true;
- }
+console.log('MAIL');
+console.log(moderateEmail("ESSch@yandex.ru"));
+console.log(moderateEmail("ESSch@sub.yandex.ru"));
+console.log(moderateEmail("ё@yandex.ru"));
+console.log(!moderateEmail("mail.ya"));
+console.log(!moderateEmail(""));
+console.log(!moderateEmail("yandex.ru"));
 
- console.log('MAIL');
- console.log(moderateEmail("ESSch@yandex.ru"));
- console.log(moderateEmail("ESSch@sub.yandex.ru"));
- console.log(moderateEmail("ё@yandex.ru"));
- console.log(!moderateEmail("mail.ya"));
- console.log(!moderateEmail(""));
- console.log(!moderateEmail("yandex.ru"));
+function moderatePhone(phone) {
+    if (undefined == phone) {
+        return false;
+    }
 
- function moderatePhone(phone)
- {
- if(null === phone.match(/^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/)) {
- return false;
- }
+    if (null === phone.match(/^\+7\(\d{3}\)\d{3}-\d{2}-\d{2}$/)) {
+        return false;
+    }
 
- var digits = phone.match(/\d/g);
+    var digits = phone.match(/\d/g);
 
- if (null === digits) {
- return false;
- }
+    if (null === digits) {
+        return false;
+    }
 
- var sum = digits.reduce(function (sum, digit) {
- return +sum + +digit;
- }, 0);
+    var sum = digits.reduce(function (sum, digit) {
+        return +sum + +digit;
+    }, 0);
 
- if (sum > 30) {
- return false;
- }
+    if (sum > 30) {
+        return false;
+    }
 
- return true;
- }
- console.log("phone");
- console.log(moderatePhone("+7(111)111-11-11"));
- console.log(!moderatePhone("+7(111)111-11-111"));
- console.log(!moderatePhone("+7(333)334-22-33"));
- console.log(!moderatePhone("+7(11a)334-22-33"));
- */
+    return true;
+}
+console.log("phone");
+console.log(moderatePhone("+7(111)111-11-11"));
+console.log(!moderatePhone("+7(111)111-11-111"));
+console.log(!moderatePhone("+7(333)334-22-33"));
+console.log(!moderatePhone("+7(11a)334-22-33"));
